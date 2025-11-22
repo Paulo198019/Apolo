649e592dd29838f67cfdb2fe5028a0b6f40399b3
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2025-11-22 00:29:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// Configuração do Vite para GitHub Pages
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Apolo/', // Apolo
-})
+  base: '/Apolo/', // Nome do repositório no GitHub
+});
